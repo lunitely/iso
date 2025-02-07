0c1fa59aff07a0eb6acc847b5ab81e418900b8d8
Hackrland <contact@hackrland.dev>
2025-02-07 17:46:45 +0100

    Updated ISO

--- airootfs/home/lunitely/.bashrc
+++ airootfs/home/lunitely/.bashrc
@@ -1,2 +1,2 @@
-sudo su
-bash
+echo Switch to root: sudo su
+alias exit="echo If you are sure you want to exit type: 'unalias exit'"
--- profiledef.sh
+++ profiledef.sh
@@ -17,6 +17,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
+  ["/home/lunitely"]="1000:1000:775"
+  ["/etc/sudoers"]="0:0:750"
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.gnupg"]="0:0:700"
