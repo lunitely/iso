82197ed042e4d14669be9ea2b4b471afe9bc270e
Alois~ <moaugustin@proton.me>
2025-02-09 20:24:35 +0100

    Added Internet connection check

--- airootfs/etc/NetworkManager/dispatcher.d/99-connectivity-change.sh
+++ airootfs/etc/NetworkManager/dispatcher.d/99-connectivity-change.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 if [ "$2" = "connectivity-change" ]; then
-    /usr/bin/fix-repository &
+    if ping -c 1 1.1.1.1 &>> /var/run/fix-repository.log; then
+        /usr/bin/fix-repository &
+    fi
 fi
